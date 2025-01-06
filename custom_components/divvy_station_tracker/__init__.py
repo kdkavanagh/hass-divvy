8d02c9a9c11f767e3cf002a9b90a9c1962f8b2f3
Kyle D. Kavanagh <kdkavanagh@gmail.com>
2025-01-06 01:29:56 +0000

    Unconditionally forward setup

--- custom_components/divvy_station_tracker/__init__.py
+++ custom_components/divvy_station_tracker/__init__.py
@@ -2,28 +2,22 @@ import asyncio
 import logging
 from datetime import timedelta
 
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import (
-    CONF_ZONE,
-    Platform,
-    SERVICE_RELOAD,
-)
-from homeassistant.core import HomeAssistant
-import pybikes
 import async_timeout
-from homeassistant.helpers.update_coordinator import (
-    DataUpdateCoordinator,
-)
+import pybikes
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_ZONE, SERVICE_RELOAD, Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+
 _LOGGER = logging.getLogger(__name__)
 
 
 DOMAIN = "divvy_station_tracker"
 
-PLATFORMS=[Platform.SENSOR]
+PLATFORMS = [Platform.SENSOR]
 
-CONF_COORDINATOR ="coord"
+CONF_COORDINATOR = "coord"
 CONF_STATION_NAME = "station_name"
-
 
 
 class BikeCoordinator(DataUpdateCoordinator):
@@ -42,31 +36,28 @@ class BikeCoordinator(DataUpdateCoordinator):
             await self.hass.async_add_executor_job(self.divvy.update)
         return {x.name: x for x in self.divvy.stations}
 
+
 async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Handle removal of an entry."""
     return await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS)
-
 
 
 async def async_reload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> None:
     await async_unload_entry(hass, config_entry)
     await async_setup_entry(hass, config_entry)
 
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     entry.async_on_unload(entry.add_update_listener(async_reload_entry))
     hass.data.setdefault(DOMAIN, {})
     entry_data = hass.data[DOMAIN].setdefault(entry.entry_id, {})
-    if not entry.options:
-        return True
-
     coordinator = BikeCoordinator(hass)
     await coordinator.async_config_entry_first_refresh()
-
-    config = entry.options
     entry_data[CONF_COORDINATOR] = coordinator
-    entry_data[CONF_STATION_NAME] = config[CONF_STATION_NAME]
-    entry_data[CONF_ZONE] = config[CONF_ZONE]
-
+    if entry.options:
+        config = entry.options
+        entry_data[CONF_STATION_NAME] = config[CONF_STATION_NAME]
+        entry_data[CONF_ZONE] = config[CONF_ZONE]
 
     async def _handle_reload(service):
         """Handle reload service call."""
@@ -81,13 +72,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
         await asyncio.gather(*reload_tasks)
 
-
     hass.helpers.service.async_register_admin_service(
         DOMAIN,
         SERVICE_RELOAD,
         _handle_reload,
     )
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-
 
     return True
